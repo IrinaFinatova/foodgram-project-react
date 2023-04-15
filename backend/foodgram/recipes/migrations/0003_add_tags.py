3b34e6afee3932bb8b45effb747c7cb7e3c9a6ec
Финатова Ирина <irinafinatova@yandex.ru>
2023-04-16 00:53:46 +0300

    subscribe

---
+++ backend/foodgram/recipes/migrations/0003_add_tags.py
@@ -0,0 +1,30 @@
+from django.db import migrations
+
+
+INITIAL_TAGS = [{'color': '#20B2AA', 'name': 'Завтрак', 'slug': 'breakfast'},
+                {'color': '#FF00FF', 'name': 'Обед', 'slug': 'dinner'},
+                {'color': '#008000', 'name': 'Ужин', 'slug': 'supper'}]
+
+
+def add_tags(apps, schema_editor):
+    Tag = apps.get_model('recipes', 'Tag')
+    for tag in INITIAL_TAGS:
+        new_tag = Tag(**tag)
+        new_tag.save()
+
+
+def remove_tags(apps, schema_editor):
+    Tag = apps.get_model('recipes', 'Tag')
+    for tag in INITIAL_TAGS:
+        Tag.objects.get(slug=tag['slug']).delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('recipes', '0002_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_tags, remove_tags)
+    ]

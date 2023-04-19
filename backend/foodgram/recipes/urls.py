57d39fcaf8ebd57233922823d063808fd4fc5206
Финатова Ирина <irinafinatova@yandex.ru>
2023-04-20 01:28:16 +0300

    изменения url

--- backend/foodgram/recipes/urls.py
+++ backend/foodgram/recipes/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
@@ -21,6 +19,4 @@ urlpatterns = [
     path('', include(router.urls)),
 
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+

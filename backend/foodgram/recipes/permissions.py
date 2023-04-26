b44b65f36f60a88fff1ef6815906555d6c42f286
Финатова Ирина <irinafinatova@yandex.ru>
2023-04-26 16:24:00 +0300

    переделка Андрей

--- backend/foodgram/recipes/permissions.py
+++ backend/foodgram/recipes/permissions.py
@@ -5,10 +5,8 @@ class IsOwnerOrIsStaffPermission(BasePermission):
     """Разрешение на редактирование владельцу и персоналу,
     остальным пользователям только просмотр"""
     def has_permission(self, request, view):
-        return (
-                request.method in SAFE_METHODS
-                or request.user.is_authenticated
-        )
+        return (request.method in SAFE_METHODS
+                or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
         return (request.method in SAFE_METHODS

d8a3d7fcc3b34a1f81b3ade4f220f6f1765e40ff
Финатова Ирина <irinafinatova@yandex.ru>
2023-04-27 21:41:47 +0300

    is subscribed

--- backend/foodgram/users/urls.py
+++ backend/foodgram/users/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('users/subscription/', SubscribeViewList.as_view()),
     path('users/<int:pk>/subscribe/', subscribe),
     path(r'users/<int:pk>/', UserDetail.as_view()),
-    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]

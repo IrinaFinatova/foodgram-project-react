8663ef3a837bae64b0b7a7f458665a86402e2a2c
Финатова Ирина <irinafinatova@yandex.ru>
2023-04-27 22:11:05 +0300

    is subscribed

--- backend/foodgram/users/urls.py
+++ backend/foodgram/users/urls.py
@@ -3,9 +3,9 @@ from django.urls import include, path
 from .views import SubscribeViewList, UserDetail, subscribe
 
 urlpatterns = [
+    path('auth/', include('djoser.urls.authtoken')),
+    path('', include('djoser.urls')),
     path('users/subscription/', SubscribeViewList.as_view()),
     path('users/<int:pk>/subscribe/', subscribe),
-    path(r'users/<int:pk>/', UserDetail.as_view()),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
+    path(r'users/<int:pk>/', UserDetail.as_view())
 ]
